dd2f67eed6f1c4d54acb2880078a0407897221f3
Timothée Floure <timothee.floure@fnux.ch>
2017-02-21 22:31:10 +0100

    Initial mix project

---
+++ lib/hanabi.ex
@@ -0,0 +1,18 @@
+defmodule Hanabi do
+  @moduledoc """
+  Documentation for Hanabi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hanabi.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hanabi_test.exs
@@ -0,0 +1,8 @@
+defmodule HanabiTest do
+  use ExUnit.Case
+  doctest Hanabi
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
